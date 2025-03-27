ccba380163e1cf544a399a9c1534598b279e97ab
ramosdev <dev.ramos.pages@gmail.com>
2025-03-27 12:56:33 -0500

    refactor blog page

--- src/app/pages/resourses/blog/blog.component.ts
+++ src/app/pages/resourses/blog/blog.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroHeaderComponent } from "../../../components/hero-header/hero-header.component";
 import { BlogListComponent } from "../../../components/resources/blog-list/blog-list.component";
+import { FooterComponent } from "../../../components/footer/footer.component";
 
 @Component({
   selector: 'app-blog',
-  imports: [HeroHeaderComponent, BlogListComponent],
+  imports: [HeroHeaderComponent, BlogListComponent, FooterComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
