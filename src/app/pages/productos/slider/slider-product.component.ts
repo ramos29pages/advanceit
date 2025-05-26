aa4c2a64f17fc53b06692595842a60029aad3a52
ramosdev <dev.ramos.pages@gmail.com>
2025-05-26 09:11:00 -0500

    feat: actualizar el componente del slider de productos; mejorar la visualización de banners y ajustar imágenes

--- src/app/pages/productos/slider/slider-product.component.ts
+++ src/app/pages/productos/slider/slider-product.component.ts
@@ -15,34 +15,34 @@ export class SliderProductComponent {
       description: 'Proveemos hardware autorizado por las grandes marcas.',
       buttonText: 'Shop Now',
       image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
-      background: 'linear-gradient(to right, #7f00ff, #e100ff, #ff4e50)'
+     background: 'linear-gradient(to right, #4d0080, #990099, #cc3f41)'
     },
-    {
-      title: 'Gaming Power Unleashed',
-      description: 'Equip your setup with the best gaming hardware.',
-      buttonText: 'Explore',
-      image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
-      background: 'linear-gradient(to right, #141e30, #243b55)'
-    },
-    {
-      title: 'Student Essentials',
-      description: 'Affordable and powerful laptops for study and productivity.',
-      buttonText: 'View Offers',
-      image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
-      background: 'linear-gradient(to right, #56ccf2, #2f80ed)'
-    },
-    {
-      title: 'Designer’s Choice',
-      description: 'High-resolution screens and powerful GPUs for creatives.',
-      buttonText: 'Get Inspired',
-      image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
-      background: 'linear-gradient(to right, #ff4e50, #f9d423)'
-    },
+    // {
+    //   title: 'Gaming Power Unleashed',
+    //   description: 'Equip your setup with the best gaming hardware.',
+    //   buttonText: 'Explore',
+    //   image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
+    //   background: 'linear-gradient(to right, #141e30, #243b55)'
+    // },
+    // {
+    //   title: 'Student Essentials',
+    //   description: 'Affordable and powerful laptops for study and productivity.',
+    //   buttonText: 'View Offers',
+    //   image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
+    //   background: 'linear-gradient(to right, #56ccf2, #2f80ed)'
+    // },
+    // {
+    //   title: 'Designer’s Choice',
+    //   description: 'High-resolution screens and powerful GPUs for creatives.',
+    //   buttonText: 'Get Inspired',
+    //   image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
+    //   background: 'linear-gradient(to right, #ff4e50, #f9d423)'
+    // },
     {
       title: 'Business Ready',
       description: 'Professional laptops with enhanced security and performance.',
       buttonText: 'See Plans',
-      image: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
+      image: '/products/notebook-latitude-14-5440-nt-gray-gallery-2.avif',
       background: 'linear-gradient(to right, #232526, #414345)'
     }
   ];
