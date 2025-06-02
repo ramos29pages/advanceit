8c7f236282f8ce6a41e323fc196295dee8f456d5
ramosdev <dev.ramos.pages@gmail.com>
2025-06-02 00:31:07 -0500

    feat: agregar funcionalidad de desplazamiento para productos favoritos; implementar botones de navegación y actualizar la lista de productos

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -15,6 +15,7 @@ import {
   faCodeBranch,
   faShop,
 } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterLink } from '@angular/router';
 import { NexsysApiService } from '../../services/nexys.service';
@@ -24,14 +25,15 @@ import { ProductAdvanceComponent } from '../../components/products/product-advan
 import { CartService } from '../../services/cart.service';
 import { AdvanceProductsService } from '../../services/product.service';
 import { CategoryMenuComponent } from './categories/category.component';
-import { TrmComponent } from "../../components/navbar/trm/trm.component";
-import { SliderProductComponent } from "./slider/slider-product.component";
-import { ProductVerticalComponent } from "../../components/products/product-vertical/product-vertical.component";
-import { BrandSliderComponent } from "../../shared/brand-slider/brand-slider.component";
-import { TeamFormLiteComponent } from "../../components/team-form-lite/team-form-lite.component";
-import { SimpleCtaComponent } from "../../components/inicio/simple-cta/simple-cta.component";
-import { BuscadorPrincipalComponent } from "../../components/products/buscador-principal/buscador-principal.component";
+import { TrmComponent } from '../../components/navbar/trm/trm.component';
+import { SliderProductComponent } from './slider/slider-product.component';
+import { ProductVerticalComponent } from '../../components/products/product-vertical/product-vertical.component';
+import { BrandSliderComponent } from '../../shared/brand-slider/brand-slider.component';
+import { TeamFormLiteComponent } from '../../components/team-form-lite/team-form-lite.component';
+import { SimpleCtaComponent } from '../../components/inicio/simple-cta/simple-cta.component';
+import { BuscadorPrincipalComponent } from '../../components/products/buscador-principal/buscador-principal.component';
 import { SanitizeImageUrlPipe } from '../../pipes/sanitize-image-url.pipe';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-productos',
@@ -52,8 +54,9 @@ import { SanitizeImageUrlPipe } from '../../pipes/sanitize-image-url.pipe';
     TeamFormLiteComponent,
     SimpleCtaComponent,
     BuscadorPrincipalComponent,
-    SanitizeImageUrlPipe
-],
+    SanitizeImageUrlPipe,
+    FontAwesomeModule
+  ],
   templateUrl: './productos.component.html',
   styles: [
     `
@@ -97,6 +100,8 @@ export class ProductosComponent implements OnInit {
   faCopririgth = faCopyright;
   faCodeBranch = faCodeBranch;
   faShop = faShop;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
 
   // Arrays de filtros disponibles
   categorias: string[] = ['Computadoras', 'Accesorios', 'Monitores'];
@@ -204,13 +209,116 @@ export class ProductosComponent implements OnInit {
     },
   ];
 
+  productosFavorites: Producto[] = [
+    {
+      id: '1',
+      nombre: 'Latitude 5450 Portátil',
+      descripcion:
+        'Intel® Core™ i7-1370P, vPro® de 13.ª generación (14 núcleos, hasta 5,2 GHz de frecuencia Turbo) ',
+      precio: 1499.99,
+      imagen: '/products/notebook-latitude-14-5440-nt-gray-gallery-2.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        'Intel® Core™ Ultra 5 135U, vPro®',
+        'Windows 11 Pro',
+        'Intel® Graphics',
+        '16 GB DDR5 | 512 GB SSD | 14.0-in. display Full HD (1920X1080)',
+      ],
+      categoria: 'Computadoras',
+      etiquetas: ['Nuevo', 'Popular'],
+    },
+    {
+      id: '2',
+      nombre: 'Latitude 7450 Laptop or 2-in-1',
+      descripcion:
+        '14-inch premium AI laptop or 2-in-1 featuring 16:10 displays, enhanced audio, ultralight option and Intel® Core™ Ultra processor.',
+      precio: 999.99,
+      imagen: '/products/notebook-latitude-14-7450-t-gray-gallery-1.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        'Intel® Core™ Ultra 7 165U, vPro®',
+        'Windows 11 Pro',
+        'Intel® Graphics',
+        '16 GB LPDDR5X | 256 GB SSD | 14" Non-Touch FHD+ (1920x1200)',
+      ],
+      categoria: 'Computadoras',
+      etiquetas: ['Nuevo'],
+    },
+    {
+      id: '3',
+      nombre: 'Dell Pro Wired ANC Headset - WH5024',
+      descripcion:
+        'Elevate your workday communication with this headset that comes equipped with an AI-based microphone and Active Noise Cancellation, designed to reduce background noise, ensure comfort, and bring your productivity to the next level.',
+      precio: 199.99,
+      imagen: '/products/accessories-dell-wh5024-anc-bk-gallery-1.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        'Microsoft Teams (Open Office) Certified, Zoom Certified',
+        '3 Year Limited Hardware with Advanced Exchange Service',
+        'Win11/10 64 Bit, Mac OS',
+      ],
+      categoria: 'Accesorios',
+      etiquetas: ['Recomendado', 'Sonido'],
+    },
+    {
+      id: '4',
+      nombre: 'Dell Pro 24 Plus Monitor - P2425H',
+      descripcion: 'In-Plane Switching (IPS) technology | 1920 x 1080',
+      precio: 399.99,
+      imagen: '/products/monitor-p2425h-black-gallery-2.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        'In-Plane Switching (IPS) technology',
+        'Resolution / Refresh Rate 1920 x 1080',
+        'Adjustability Height, Tilt, Swivel, Pivot',
+        'Diagonal Size 23.8',
+      ],
+      categoria: 'Monitores',
+      etiquetas: ['Nuevo'],
+    },
+    {
+      id: '5',
+      nombre: 'Dell Pro Dock - WD25',
+      descripcion:
+        'Boost your productivity with the latest pro dock that offers up to 100W power delivery and a wide variety of connecting options.',
+      precio: 129.99,
+      imagen: '/products/dock-station-wd25-black-gallery-1.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        '100W (Dell systems) 96W (non-Dell systems)',
+        'RJ45 Ethernet port, 2.5GbE',
+        '3-Year Limited Hardware Warranty with Advanced Exchange Additional 4- & 5-year warranty optional',
+      ],
+      categoria: 'Accesorios',
+      etiquetas: ['Popular'],
+    },
+    {
+      id: '6',
+      nombre: 'Dell Premier Multi-Device Wireless Keyboard and Mouse – KM7321W',
+      descripcion:
+        'Experience superior multitasking features with a stylish and comfortable premium keyboard and mouse combo. Complete your tasks powered by one of the industry’s leading battery lives at up to 36 months.',
+      precio: 59.99,
+      imagen: '/products/km7321w-xkb-01-gy.avif',
+      marca: 'Dell',
+      caracteristicas: [
+        'USB wireless receiver',
+        'Adjustable DPI. 1000, 1600(default), 2400, 4000',
+        '12 programmable keys of F1-F12',
+      ],
+      categoria: 'Accesorios',
+      etiquetas: ['Oferta', 'Popular'],
+    },
+  ];
+
+  @ViewChild('favoritesContainer')
+  favoritesContainer!: ElementRef<HTMLDivElement>;
   filteredProducts = signal<Producto[]>([]);
 
   constructor(
     private nexsysService: NexsysApiService,
     private ingramService: IngramService,
     private cartService: CartService,
-    private productService: AdvanceProductsService,
+    private productService: AdvanceProductsService
   ) {
     // En el constructor de NavbarComponent
     cartService.getCart().subscribe((items) => {
@@ -224,6 +332,17 @@ export class ProductosComponent implements OnInit {
     setInterval(() => {
       this.setRandomWelcomeMessage();
     }, 5000);
+  }
+
+  scrollFavorites(direction: 'left' | 'right') {
+    const container = this.favoritesContainer?.nativeElement;
+    if (!container) return;
+
+    const scrollAmount = 300;
+    container.scrollBy({
+      left: direction === 'left' ? -scrollAmount : scrollAmount,
+      behavior: 'smooth',
+    });
   }
 
   setRandomWelcomeMessage() {
@@ -360,17 +479,17 @@ export class ProductosComponent implements OnInit {
   }
 
   onMenucategories(): void {
-  this.menuCategories = !this.menuCategories;
+    this.menuCategories = !this.menuCategories;
 
-  const html = document.documentElement;
-  const body = document.body;
+    const html = document.documentElement;
+    const body = document.body;
 
-  if (this.menuCategories) {
-    html.classList.add('no-scroll');
-    body.classList.add('no-scroll');
-  } else {
-    html.classList.remove('no-scroll');
-    body.classList.remove('no-scroll');
+    if (this.menuCategories) {
+      html.classList.add('no-scroll');
+      body.classList.add('no-scroll');
+    } else {
+      html.classList.remove('no-scroll');
+      body.classList.remove('no-scroll');
+    }
   }
-}
 }
